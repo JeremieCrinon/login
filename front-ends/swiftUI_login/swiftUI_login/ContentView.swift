f61aa026edd5b962d1a52da44c782f0d10a9ce2f
Jérémie Crinon <jeremie@jcrinon.com>
2025-06-27 18:56:29 +0200

    Adding a loadingManager that can be edited to tell if something is loading plus a loading view that is displayed when loading, that prevents the user from doing something and displays a loading thing.

--- front-ends/swiftUI_login/swiftUI_login/ContentView.swift
+++ front-ends/swiftUI_login/swiftUI_login/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var loadingManager = LoadingManager.preview()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Group {
+                Text("Hello, World !")
+            }
+            
+            Loading()
+                .environmentObject(loadingManager)
         }
-        .padding()
     }
 }
 
---
+++ front-ends/swiftUI_login/swiftUI_login/Views/Components/Loading/Loading.swift
@@ -0,0 +1,43 @@
+//
+//  Loading.swift
+//  swiftUI_login
+//
+//  Created by Jeremie Crinon on 27/06/2025.
+//
+
+import SwiftUI
+
+struct Loading: View {
+    @EnvironmentObject var loadingManager: LoadingManager
+    
+    var body: some View {
+        if loadingManager.isLoading {
+            HStack {
+                ProgressView()
+                    .scaleEffect(2)
+                    .padding()
+                
+                Text("loading", comment: "The text in the loading view next to the ProgressView")
+                    .font(.title)
+                    .padding()
+            }
+            .font(.title)
+            .padding()
+            .glassEffect()
+            .ignoresSafeArea()
+            
+                
+        }
+    }
+}
+
+#Preview {
+    ZStack { // We put a Zstack here to have a similar displaying as in the complete app
+        Text("Preview")
+        #if DEBUG // We add this condition, even tough it should always be true, to really make sure we display this only in debug mode, as the preview function of loading manager is only avaible in debug mode
+            Loading()
+                .environmentObject(LoadingManager.preview())
+        #endif
+    }
+    
+}
