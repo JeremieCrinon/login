4cdf9418db57332592b0e4b6f7e4ba7c389f242b
Jérémie Crinon <jeremie@jcrinon.com>
2025-06-27 18:12:57 +0200

    Making a plain swiftUI IOS app.

---
+++ front-ends/swiftUI_login/swiftUI_login/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftUI_login
+//
+//  Created by Jeremie Crinon on 27/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
