8a32a04decd0614419254be6a21318477cefab35
Jérémie Crinon <accounts@jcrinon.com>
2025-11-02 14:56:40 +0100

    Adding the missing unit tests.

--- back-ends/vapor_login/Tests/AppTests/usersRoutesTests.swift
+++ back-ends/vapor_login/Tests/AppTests/usersRoutesTests.swift
@@ -2,6 +2,7 @@
 import VaporTesting
 import Testing
 import JWTKit
+import struct Foundation.UUID
 
 @Suite("users routes tests")
 struct UsersRoutesTests {
@@ -59,6 +60,82 @@ struct UsersRoutesTests {
             }, afterResponse: { res async throws in
                 #expect(res.status == .ok)
             })
+        }
+    }
+
+    @Test("Test user deletion")
+    func testUserDeletion() async throws {
+        try await withAppIncludingDB { app in
+            let token = try await createTestUserAndGetJWT(app: app, roles: [.admin])
+            let user = try await createTestUser(on: app.db, roles: [], email: "delete@mail.com")
+            let id: UUID = user.id!
+
+            try await app.testing().test(.DELETE, "users/\(id)", beforeRequest: { req in
+                req.headers.add(name: "authorization", value: "Bearer \(token)")
+            }, afterResponse: { res async throws in
+                #expect(res.status == .ok)
+            })
+        }
+    }
+
+    @Test("Test user email editing with an already existing email")
+    func testUserEmailEditingExistingEmail() async throws {
+        try await withAppIncludingDB { app in
+            let token = try await createTestUserAndGetJWT(app: app, roles: [.admin])
+            let user = try await createTestUser(on: app.db, roles: [], email: "editemail@mail.com")
+            let id: UUID = user.id!
+            
+            let body = UsersController.editUserEmailRequest(
+                email: "test@mail.com" // The email already taken by the user we got the JWT from
+            )
+
+            try await app.testing().test(.PUT, "users/\(id)/email/en", beforeRequest: { req in 
+                try req.content.encode(body)
+                req.headers.add(name: "authorization", value: "Bearer \(token)")
+            }, afterResponse: { res async throws in 
+                #expect(res.status == .conflict)
+            })
+        }
+    }
+
+    @Test("Test user email editing with right values")
+    func testUserEmailEditingRightValues() async throws {
+        try await withAppIncludingDB { app in
+            let token = try await createTestUserAndGetJWT(app: app, roles: [.admin])
+            let user = try await createTestUser(on: app.db, roles: [], email: "editemail@mail.com")
+            let id: UUID = user.id!
+            
+            let body = UsersController.editUserEmailRequest(
+                email: "unique@mail.com"
+            )
+
+            try await app.testing().test(.PUT, "users/\(id)/email/en", beforeRequest: { req in 
+                try req.content.encode(body)
+                req.headers.add(name: "authorization", value: "Bearer \(token)")
+            }, afterResponse: { res async throws in 
+                #expect(res.status == .ok)
+            })
+        }
+    }
+
+    @Test("Test user roles editing")
+    func testUserRolesEditing() async throws {
+        try await withAppIncludingDB { app in 
+            let token = try await createTestUserAndGetJWT(app: app, roles: [.admin])
+            let user = try await createTestUser(on: app.db, roles: [], email: "editemail@mail.com")
+            let id: UUID = user.id!
+            
+            let body = UsersController.editUserRolesRequest(
+                roles: [.user]
+            )
+
+            try await app.testing().test(.PUT, "users/\(id)/roles", beforeRequest: { req in 
+                try req.content.encode(body)
+                req.headers.add(name: "authorization", value: "Bearer \(token)")
+            }, afterResponse: { res async throws in 
+                #expect(res.status == .ok)
+            })
+
         }
     }
 }
